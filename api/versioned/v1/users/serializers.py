0f614e7fbbaa66b9ce9f59bfeaa7e5061dbab655
kkamikoon <rootsik1221@gmail.com>
2023-07-05 18:54:57 +0900

    UPDATE Mixins and logger & UPDATE useless fields in serializers

--- api/versioned/v1/users/serializers.py
+++ api/versioned/v1/users/serializers.py
@@ -6,9 +6,11 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+        read_only_fields = ('id', 'last_login', 'date_joined', 'last_password_change', 'groups',
+                            'user_permissions', 'is_online', )
 
 
 class UserRetrieveSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("id", 'email', 'is_staff', 'is_superuser', 'date_joined', 'last_login')
+        fields = ("id", 'email', 'is_staff', 'is_superuser', 'date_joined', 'last_login', )
--- common/mixins.py
+++ common/mixins.py
@@ -1,6 +1,7 @@
 import json
 import logging
 from collections import OrderedDict
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import mixins, GenericViewSet
 from rest_framework.utils.serializer_helpers import ReturnDict, ReturnList
@@ -9,62 +10,70 @@ from rest_framework.utils.serializer_helpers import ReturnDict, ReturnList
 logger = logging.getLogger("django.server")
 
 
-class LoggerMixin:        
-    def request_logger(self, class_name: str, function_name: str, payload: OrderedDict = None):
-        formatted_string = f"Request({function_name}) : {class_name} / {payload}"
+class LoggerMixin:
+    def request_logger(self: GenericViewSet, payload: OrderedDict = None):
+        formatted_string = f"Request({self.action} - {self.request._request.path}) : {str(self.__class__)} / {payload}"
         logger.info(formatted_string)
 
-    def response_logger(self, class_name:str, function_name: str, payload: ReturnDict):
-        if function_name == "list":
+    def response_logger(self: GenericViewSet, payload: ReturnDict = None):
+        if self.action == "list":
             # If too much data logged, customize this function.
             payload = json.loads(json.dumps(payload))
+        elif not payload:
+            pass
         else:
             payload = dict(payload)
-        formatted_string = f"Response({function_name}) : {class_name} / {payload}"
+        formatted_string = f"Response({self.action} - {self.request._request.path}) : {str(self.__class__)} / {payload}"
         logger.info(formatted_string)
         
 
 class ListModelMixin(mixins.ListModelMixin, LoggerMixin, GenericViewSet):
     def list(self, request, *args, **kwargs):
-        self.request_logger(class_name=self.__class__, function_name="list", payload=request.data)
+        self.request_logger(payload=request.data)
         queryset = self.filter_queryset(self.get_queryset())
 
         page = self.paginate_queryset(queryset)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
-            self.response_logger(class_name=self.__class__, function_name="list", payload=serializer.data)
+            self.response_logger(payload=serializer.data)
             return self.get_paginated_response(serializer.data)
 
         serializer = self.get_serializer(queryset, many=True)
-        self.response_logger(class_name=self.__class__, function_name="list", payload=serializer.data)
+        self.response_logger(payload=serializer.data)
         return Response(serializer.data)
 
 
-class RetrieveModelMixin(mixins.RetrieveModelMixin, GenericViewSet):
-    def retrieve(self: GenericViewSet, request, *args, **kwargs):
-        self.request_logger(class_name=self.__class__, function_name="retrieve")
+class RetrieveModelMixin(mixins.RetrieveModelMixin, LoggerMixin, GenericViewSet):
+    def retrieve(self, request, *args, **kwargs):
+        self.request_logger()
         instance = self.get_object()
         serializer = self.get_serializer(instance)
-        self.response_logger(class_name=self.__class__, function_name="retrieve", payload=serializer.data)
-        return Response(serializer.data, status=200)
+        self.response_logger(payload=serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class CreateModelMixin(mixins.CreateModelMixin, GenericViewSet):
-    def perform_create(self: GenericViewSet, serializer):
+class CreateModelMixin(mixins.CreateModelMixin, LoggerMixin, GenericViewSet):
+    def perform_create(self, serializer):
+        self.request_logger(payload=self.request.data)
         serializer.save()
+        self.response_logger()
 
 
-class UpdateModelMixin(mixins.UpdateModelMixin, GenericViewSet):
-    def perform_update(self: GenericViewSet, serializer):
+class UpdateModelMixin(mixins.UpdateModelMixin, LoggerMixin, GenericViewSet):
+    def perform_update(self, serializer):
+        self.request_logger(payload=self.request.data)
         serializer.save()
+        self.response_logger(payload=serializer.data)
 
 
-class DestroyModelMixin(mixins.DestroyModelMixin, GenericViewSet):
-    def perform_delete(self: GenericViewSet, instance):
+class DestroyModelMixin(mixins.DestroyModelMixin, LoggerMixin, GenericViewSet):
+    def perform_destroy(self, instance):
+        self.request_logger(payload=instance)
         instance.delete()
+        self.response_logger()
 
 
-class MappingViewSetMixin(object):
+class MappingViewSetMixin(GenericViewSet):
     serializer_action_map = {}
 
     def get_serializer_class(self):
